28e74be6b7e7bcf8e02a5298c7a3da503fa9246e
yourname <weijie@weijiedeMacBook-Pro.local>
2020-03-01 14:25:32 +0800

    add icons

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -22,7 +22,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  @import '~styles/iconfont/varibles.styl'
+  @import '~styles/varibles.styl'
   .header
     display:flex
     line-height :.86rem
