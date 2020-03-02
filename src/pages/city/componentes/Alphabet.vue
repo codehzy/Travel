8843ef075174e5be0434be185befa96f470614ef
yourname <weijie@weijiedeMacBook-Pro.local>
2020-03-02 18:35:37 +0800

    components city

--- src/pages/city/componentes/Alphabet.vue
+++ src/pages/city/componentes/Alphabet.vue
@@ -31,8 +31,13 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   methods: {
     handleLetterClick (e) {
@@ -43,11 +48,16 @@ export default {
     },
     handleTouchMove (e) {
       if (this.touchStatus) {
-        const touchY = e.touches[0].clientY - 74
-        const index = Math.floor((touchY - 74) / 20)
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index])
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+        this.Timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 74
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
       }
     },
     handleTouchEnd () {
