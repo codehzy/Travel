23832c4f6706dae5661db5e88db30f5fffc76e95
yourname <weijie@weijiedeMacBook-Pro.local>
2020-03-02 17:11:21 +0800

    ajax city

--- src/pages/city/componentes/Alphabet.vue
+++ src/pages/city/componentes/Alphabet.vue
@@ -1,19 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item,key) of cities" :key="key">{{key}}</li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'CityAlphabet'
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
@@ -24,7 +20,7 @@ export default {
     flex-direction :column
     justify-content :center
     position :absolute
-    top:4.5rem
+    top:3rem
     right :0
     buttm:0
     width:.4rem
--- src/pages/city/componentes/list.vue
+++ src/pages/city/componentes/list.vue
@@ -12,70 +12,19 @@
     <div class="area">
       <div class="title border-topbottom">热门城市</div>
       <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+        <div class="button-wrapper" v-for="item of hot" :key="item.id">
+          <div class="button">{{item.name}}</div>
         </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+      </div>
+    <div class="area" v-for="(item,key) of cities" :key="key">
+      <div class="title border-topbottom">{{key}}</div>
+      <div class="item-list">
+        <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
+          {{innerItem.name}}
         </div>
       </div>
     </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-        <div class="item border-bottom">阿拉尔</div>
-      </div>
-    </div>
+  </div>
   </div>
   </div>
 </template>
@@ -83,6 +32,10 @@
 <script>
 import Bscroll from 'better-scroll'
 export default {
+  props: {
+    hot: Array,
+    cities: Object
+  },
   name: 'CityList',
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
@@ -114,7 +67,7 @@ export default {
       color:#666
       font-size :.26rem
     .button-list
-      padding :.1rem .6rem .1r
+      padding :.1rem .6rem .1rem .1rem
       overflow :hidden
       .button-wrapper
         float :left
